80254aab6aa35f0de224db45628a5a0102a42448
Salmaabdelaziz271 <salmayaraaa@gmail.com>
2023-12-29 21:17:17 +0200

    update shipment controller

--- src/main/java/com/ordersManagment/Service/Controller/ShipmentController.java
+++ src/main/java/com/ordersManagment/Service/Controller/ShipmentController.java
@@ -1,9 +1,12 @@
 package com.ordersManagment.Service.Controller;
 
+import com.ordersManagment.Service.Model.Shipment;
 import com.ordersManagment.Service.Response.ShipmentResponse;
 import com.ordersManagment.Service.Service.ShipmentService;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
 
 @RestController
 @AllArgsConstructor
@@ -13,26 +16,27 @@ public class ShipmentController {
     private final ShipmentService shipmentService;
 
     // Place Simple Order Shipment
-    @PostMapping("/place/{orderID}")
+    @PostMapping(value = "/place/simple/{orderID}")
     public ShipmentResponse placeSimpleOrderShipment(@PathVariable("orderId") int orderID) {
         return shipmentService.shipSimpleOrder(orderID);
     }
 
     // Place Compound Order Shipment
-    @PostMapping("/place/compound/{orderID}")
+    @PostMapping(value = "/place/compound/{orderID}")
     public ShipmentResponse placeCompoundOrderShipment(@PathVariable("orderId") int orderID) {
         return shipmentService.shipCompoundOrder(orderID);
     }
 
     // Cancel Simple Order Shipment
-    @DeleteMapping ("/cancel/{shipmentID}")
+    @DeleteMapping (value = "/cancel/simple/{shipmentID}")
     public ShipmentResponse cancelSimpleOrderShipment(@PathVariable("shipmentID") int shipmentID) {
         return shipmentService.cancelSimpleOrderShipment(shipmentID);
     }
 
     // Cancel Compound Order Shipment
-    @DeleteMapping("/cancel/compound/{shipmentID}")
+    @DeleteMapping(value ="/cancel/compound/{shipmentID}")
     public ShipmentResponse cancelCompoundOrderShipment(@PathVariable("shipmentID") int shipmentID) {
         return shipmentService.cancelCompoundOrderShipment(shipmentID);
     }
+
 }
--- src/main/java/com/ordersManagment/Service/Service/ShipmentService.java
+++ src/main/java/com/ordersManagment/Service/Service/ShipmentService.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.sql.Time;
+import java.util.ArrayList;
 import java.util.Date;
 
 @Service
