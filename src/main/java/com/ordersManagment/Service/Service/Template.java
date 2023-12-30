c2d906d1cbdb4133b4b35722845744ec50d1f157
ahmedsaad123456 <102562587+ahmedsaad123456@users.noreply.github.com>
2023-12-30 16:30:06 +0200

    edit notification

--- src/main/java/com/ordersManagment/Service/Service/Template.java
+++ src/main/java/com/ordersManagment/Service/Service/Template.java
@@ -1,26 +1,42 @@
 package com.ordersManagment.Service.Service;
 
+import com.ordersManagment.Service.Database.CustomerDB;
 import com.ordersManagment.Service.Model.Customer;
+import com.ordersManagment.Service.Model.Order;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Service;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
+@Service
+
+
+
 public abstract class Template {
-    private Customer customer;
+
+
+    private Order order;
+
+
+
+
 
     public abstract String getMessage();
 
     protected String replacePlaceholders(String template) {
+        Customer customer = CustomerDB.getCustomerByID(order.getCustomer().getID());
         // Replace {customerName} with actual customer name
         template = template.replace("{customerName}", customer.getName());
 
         // Replace {orderID} with actual order ID
-        template = template.replace("{orderID}", String.valueOf(customer.getOrderID()));
+        template = template.replace("{orderID}", String.valueOf(order.getOrderID()));
 
         // Replace {customerBalance} with actual customer balance
         template = template.replace("{customerBalance}", String.valueOf(customer.getBalance()));
